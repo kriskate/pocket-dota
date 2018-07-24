4e41eda2a3b6450ad943757183c7d5cf9ffcb2dc
kristi <kristi.stanescu@gmail.com>
2018-07-24 14:29:04 +0300

    refactored wiki reducer

--- reducers/wiki.js
+++ reducers/wiki.js
@@ -1,27 +1,49 @@
-const ActionTypes = {
+import { model_data } from "../constants/Models";
+
+const Types = {
+  NEW_WIKI: 'NEW_WIKI_AVAILABLE',
   NEW_WIKI: 'NEW_WIKI',
 
-  LOADED: 'LOADED',
   LOADING: 'LOADING',
-  
+
+  DOWLOADING: 'DOWLOADING',
 }
-const Actions = {
-  newWiki: data => ({ type: ActionTypes.NEW_WIKI, data })
+export const DOWNLOAD_REASONS = {
+  // no data is present
+  FRESH: 'FRESH', 
+  // data is partially corrupted (ie user cleared app cache, but intrerupted it)
+  MISSING: 'MISSING',
+  // there is a new wiki version and the user accepted the update download
+  UPDATE: 'UPDATE', 
+}
+export const Actions = {
+  newWiki: data => ({ type: Types.NEW_WIKI, data }),
+
+  loadLocalData: () => ({ type: Types.LOADING }),
+
+  downloadNewData: downloadingReason => ({ type: Types.DOWLOADING, downloadingReason }),
 }
 
 const initialState = {
-  heroes: null,
-  items: null,
-  tips: null,
-  patch_notes: null,
+  data: {},
+
+  loading: false,
+  downloading: false,
+
+  // fresh/ missing/ update
+  downloadingReason: '',
 }
 
 export default function reducer(state=initialState, action) {
   switch(action.type) {
-    case ActionTypes.NEW_WIKI:
+    case Types.NEW_WIKI:
       const { data } = action;
-      return { ...state, data };
-    break;
+      return { ...state, data, downloading: false, loading: false };
+    case Types.LOADING:
+      return { ...state, loading: true, downloading: false };
+    case Types.DOWLOADING:
+      const { downloadingReason } = action;
+      return { ...state, loading: false, downloading: true, downloadingReason };
     default:
       return state;
   }
