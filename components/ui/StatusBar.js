36c8c12a17385d7840596129b47a6865409d1a7b
kristi <kristi.stanescu@gmail.com>
2018-07-24 14:27:36 +0300

    new component - custom StatusBar

---
+++ components/ui/StatusBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, StatusBar, View } from 'react-native';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import Colors from '../../constants/Colors';
+
+const bkColor = Colors.dota_ui2;
+export default Bar = ({ backgroundColor=bkColor, ...props }) => (
+  <View style={[styles.statusBar, { backgroundColor }]}>
+    <StatusBar translucent barStyle={'light-content'} />
+  </View>
+);
+
+
+
+const styles = StyleSheet.create({
+  statusBar: {
+    flex: 1,
+    height: getStatusBarHeight(),
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    opacity: 0.5,
+    width: '100%',
+  },
+})
