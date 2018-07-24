e65480387b90c5644491a167defa12cb82f2c30f
kristi <kristi.stanescu@gmail.com>
2018-07-24 14:31:46 +0300

    HeroScreen - initial layout

--- screens/HeroScreen.js
+++ screens/HeroScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Content, Text, } from 'native-base';
+import { View, Image, StyleSheet } from 'react-native';
 
 import ButtonHamburger from '../components/ButtonHamburger';
 
@@ -10,16 +11,47 @@ export default class Screen extends React.Component {
   });
 
   render() {
-    const hero =  this.props.navigation.getParam('hero');
+    const { name, bio, hype, img_vert, abilities } =  this.props.navigation.getParam('hero');
 
     return (
       <Container>
         <Content padder>
-          <Text>
-            {hero.name}
-          </Text>
+          <View style={styles.hype}>
+            <Text>{hype}</Text>
+          </View>
+          <Image style={styles.imgHero} source={{ uri: img_vert }} />
+          <View style={styles.lore}>
+            <Text>{bio}</Text>
+          </View>
+          <View style={styles.abilities}>
+            { abilities.map(ability => (
+              <View key={ability.tag}>
+                <Image style={styles.imgAbility} source={{ uri: ability.img }} /><Text>{ability.name}</Text>
+              </View>
+            )) }
+          </View>
         </Content>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  imgHero: {
+    width: 235,
+    height: 272,
+  },
+  imgAbility: {
+    width: 90,
+    height: 90,
+  },
+  hype: {
+
+  },
+  lore: {
+
+  },
+  abilities: {
+
+  },
+})
