d0fc3e4610205a170822d29b7d58df15c840dd2c
kristi <kristi.stanescu@gmail.com>
2018-07-24 14:34:56 +0300

    Container - refactored; Text - tweaked

--- components/ui/Container.js
+++ components/ui/Container.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Colors from '../../constants/Colors';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
-export default ({ style, children }) => (
-  <View style={style ? [styles.container, style] : styles.container}>{children}</View>
-)
+export default ({ style, children, padTop, padInner }) => {
+  const padingTop = padTop ? getStatusBarHeight() : 0;
+  const padding = padingTop ? 5 : 0;
+  return <View style={[styles.container, style, padingTop, padding]}>{children}</View>
+}
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.dota_ui1,
     flex: 1,
-    padding: 5,
     // height: Platform.OS === "ios" ? deviceHeight : deviceHeight - 20
   }
 })
--- components/ui/Text.js
+++ components/ui/Text.js
@@ -3,11 +3,12 @@ import { Text, StyleSheet } from 'react-native';
 import Colors from '../../constants/Colors';
 
 export default ({ style, children }) => (
-  <Text style={style ? [styles.text, style] : styles.text}>{children}</Text>
+  <Text {...this.props} style={[style, styles.text]}>{children}</Text>
 )
 
 const styles = StyleSheet.create({
   text: {
     color: Colors.dota_red_light,
+    fontFamily: 'space-mono',
   }
 })
