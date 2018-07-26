25db8a9275e7b514fc29b47052c87da39b15d79e
kristi <kristi.stanescu@gmail.com>
2018-07-26 23:45:57 +0300

    feat(Container): prop scrollable - can now return a ScrollView

--- components/ui/Container.js
+++ components/ui/Container.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Colors from '../../constants/Colors';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { ScrollView } from '../../node_modules/react-native-gesture-handler';
 
-export default ({ style, children, padTop, padInner }) => {
+export default ({ style, children, padTop, padInner, scrollable }) => {
   const paddingTop = padTop ? getStatusBarHeight() : 0;
   const padding = padInner ? 5 : 0;
-  return <View style={[styles.container, style, {paddingTop}, {padding}]}>{children}</View>
+  const _style = [styles.container, {paddingTop, padding}, style];
+
+  return scrollable 
+    ? <ScrollView>
+        <View style={_style}>{children}</View>
+      </ScrollView>
+    : <View style={_style}>{children}</View>
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.dota_ui1,
     flex: 1,
+
     // height: Platform.OS === "ios" ? deviceHeight : deviceHeight - 20
   }
 })
