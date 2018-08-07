9a908ed96da6a66dfc3ce437b64a10e34ac25ac9
kristi <kristi.stanescu@gmail.com>
2018-08-07 19:49:43 +0300

    feat(Card) - collapsible

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,8 +1,9 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import { View, StyleSheet, Platform } from "react-native";
 import Colors from "../../constants/Colors";
 import Text from "./Text";
 import Button from "../Button";
+import Layout from "../../constants/Layout";
 
 export default class Card extends Component {
   state = {
@@ -12,32 +13,48 @@ export default class Card extends Component {
     this.setState({ collapsed: !this.state.collapsed });
   }
   render() {
-    const { style, children, collapsedTitle, noStyle } = this.props;
+    const { style, children, collapsedTitle, title } = this.props;
     const { collapsed } = this.state;
 
     return (
-      <View style={[noStyle ? null : styles.container, style]}>
-        {
-          !collapsedTitle ? {children} :
-
-          <View>
-            { this.state.collapsed ? <Text>{collapsedTitle}</Text> : {children} }
-            <Button onPress={this._toggleCollapse}>{ collapsed ? '+' : '-' }</Button>
-          </View>
+      <View>
+        { !collapsedTitle ? null :
+          <Button style={styles.btnCollapse} onPress={this._toggleCollapse}><Text>{ collapsed ? '+' : '-' }</Text></Button>
         }
+        <View style={[styles.container, style]}>
+          { !title || collapsed ? null : <Text style={styles.title}>{title}</Text> }
+          { collapsed ? <Text>{collapsedTitle}</Text> : children }
+
+        </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  title: {
+    flex: 3,
+    width: '100%',
+    
+  },
+  btnCollapse: {
+    position: 'absolute',
+    elevation: 1,
+    zIndex: 10,
+    top: 0,
+    right: 0,
+    width: 20,
+    height: 20,
+    backgroundColor: Colors.dota_background_light,
+  },
   container: {
-    backgroundColor: Colors.dota_ui1,
+    overflow: 'visible',
+    backgroundColor: Colors.dota_ui,
     borderColor: Colors.dota_ui2,
     borderWidth: 1,
-    padding: 15,
-    margin: 15,
-    marginBottom: 0,
+    padding: Layout.paddingSmall,
+    margin: Layout.paddingSmall,
+    marginBottom: Layout.paddingSmall/2,
     ...Platform.select({
       ios: {
         shadowColor: 'rgba(0,0,0, .2)',
