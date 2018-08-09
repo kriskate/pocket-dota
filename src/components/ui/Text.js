b3644775a0c0bc7975e659a1c2e0f5793690a3a7
kristi <kristi.stanescu@gmail.com>
2018-08-09 23:46:15 +0300

    feat(Text): prop labelBottom will make the text have a transparent background and be positioned absolute with bottom:0

--- src/components/ui/Text.js
+++ src/components/ui/Text.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 import Colors from '../../constants/Colors';
 
-export default ({ style, children }) => (
-  <Text {...this.props} style={[styles.text, style]}>{children}</Text>
+export default ({ style, children, labelBottom }) => (
+  <Text {...this.props} style={[styles.text, labelBottom ? styles.labelBottom : {}, style]}>{children}</Text>
 )
 
 const styles = StyleSheet.create({
   text: {
     color: Colors.dota_red_light,
-  }
+  },
+  labelBottom: {
+    textAlign: 'center',
+    color: Colors.dota_white,
+    backgroundColor: Colors.dota_ui1 + '99',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    borderWidth: 2,
+    borderColor: Colors.dota_ui1,
+  },
 })
