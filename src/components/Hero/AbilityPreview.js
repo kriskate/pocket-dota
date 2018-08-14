4a0f30a725675c395411af3db9e900330e944a63
kristi <kristi.stanescu@gmail.com>
2018-08-14 18:30:51 +0300

    refactor(AbilityPreview): added custom colors, unified HTML loading, trimmed br’s and spaces

--- src/components/Hero/AbilityPreview.js
+++ src/components/Hero/AbilityPreview.js
@@ -4,37 +4,57 @@ import { View, Image, StyleSheet, WebView } from 'react-native';
 import { url } from '../../constants/Data';
 import { Text } from '../ui';
 import { model_ability } from '../../constants/Models';
-import HTML from 'react-native-render-html';
+import RenderHTML from 'react-native-render-html';
 import Colors from '../../constants/Colors';
 import Layout from '../../constants/Layout';
+import { trimAbilities } from '../../utils/utils';
 
 
-const _html = (text) => `<span style="color:${Colors.dota_red_dark};">${text}</span>`
+const alterNode = (node) => {
+  if(node.name == 'font' && node.attribs && node.attribs.color) {
+    const color = node.attribs.color == '#bdc3c7' ? Colors.goldenrod : node.attribs.color;
+    node.attribs = { style: `color:${color};`};
+    return node;
+  }
+}
+class HTML extends React.PureComponent {
+  render() {
+    const { htmlContent, style } = this.props;
+    let trimmedHtml = trimAbilities(htmlContent);
 
+    return !trimmedHtml ? null : (
+      <RenderHTML containerStyle={[styles.html, style]} alterNode={alterNode} html={
+        `<span style="color:${Colors.dota_white};">${trimmedHtml}</span>`
+      }/>
+    )
+  }
+}
 
 export default class AbilityPreview extends React.PureComponent {
-  _alterNodeDark = (node) => {
-    if(node.name == 'font' && node.attribs && node.attribs.color) {
-      node.attribs = { style: `color:${node.attribs.color};`};
-      return node;
-    }
-  }
   
   render() {
     const ability = model_ability(this.props.ability);
     const { 
       affects, attrib, cmb, 
-      description, lore, name, notes, 
+      description, lore, name, notes,
       HasScepterUpgrade, IsGrantedByScepter
     } = ability;
     
     return (
       <View style={styles.container}>
-        <HTML containerStyle={styles.html} alterNode={this._alterNodeDark} html={_html(description)}/>
+        <HTML htmlContent={affects} />
+        <HTML htmlContent={description} style={styles.description} />
+        <HTML htmlContent={attrib} />
+        <HTML htmlContent={cmb.trim()} />
+        
+        <HTML htmlContent={notes} style={styles.description} />
+        
+        <HTML htmlContent={lore} style={styles.lore} />
+        {/* <HTML containerStyle={styles.html} alterNode={this._alterNodeDark} html={_html(description)}/>
         <HTML containerStyle={styles.html} alterNode={this._alterNodeDark} html={_html(attrib)}/>
         <HTML containerStyle={styles.html} alterNode={this._alterNodeDark} html={_html(notes)}/>
         <HTML containerStyle={styles.html} alterNode={this._alterNodeDark} html={_html(cmb)}/>
-        <HTML containerStyle={styles.html} alterNode={this._alterNodeDark} html={_html(lore)}/>
+        <HTML containerStyle={styles.html} alterNode={this._alterNodeDark} html={_html(lore)}/> */}
       </View>
     )
   }
@@ -45,11 +65,18 @@ const styles = StyleSheet.create({
     flex: 1,
     marginLeft: Layout.padding_regular,
   },
+  description: {
+    backgroundColor: Colors.dota_ui3,
+  },
+  lore: {
+    padding: 5,
+    backgroundColor: Colors.dota_red_darker,
+  },
   html: {
-    flex: 1,
+    marginVertical: 5,
     backgroundColor: Colors.dota_ui2,
     flexDirection: 'row',
     padding: Layout.padding_small,
-  }
+  },
 
 })
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,8 @@
 // to-do - add some kind of error popup to inform about this
 export const connectionIssue = () => console.log('Internet might be down');
+
+
+const r_trailingBR = new RegExp(/(<br\s*\/>)+$/);
+const r_whitespaceBR = new RegExp(/(<br\s*\/> )/, 'g');
+export const trimAbilities = (htmlContent) =>
+  htmlContent.trim().replace(r_whitespaceBR, '<br/>').replace(r_trailingBR, '');
