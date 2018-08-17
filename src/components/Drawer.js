ecb844365ad54eb67106074ac2ef851b386857b4
kristi <kristi.stanescu@gmail.com>
2018-08-17 18:09:37 +0300

    layout(Drawer): improved drawer layout, added logo

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { DrawerItems } from 'react-navigation';
-import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import { ScrollView, StyleSheet, View, Image } from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 import { SCREEN_LABELS } from '../constants/Constants';
-import { Button, Container } from './ui';
+import { Button, Container, Text } from './ui';
 import Colors from '../constants/Colors';
+import { assets } from '../constants/Data';
+import Layout from '../constants/Layout';
 
 export default class Drawer extends React.Component {
   render() {
     return (
-      <Container scrollable padTop>
+      <Container scrollable>
+        <View style={styles.imgIconWrapper}>
+          <Image style={styles.imgIcon} source={assets.app.logoRed} />
+        </View>
         <Button style={styles.item} onPress={() => this.props.navigation.navigate(SCREEN_LABELS.HOME)} >
           <Text style={[styles.label]}>{ SCREEN_LABELS.HOME }</Text>
         </Button>
@@ -22,6 +27,17 @@ export default class Drawer extends React.Component {
 
 
 const styles = StyleSheet.create({
+  imgIconWrapper: {
+    marginTop: Layout.padding_regular + getStatusBarHeight(),
+    marginVertical: Layout.padding_regular,
+    paddingVertical: Layout.padding_regular,
+  },
+  imgIcon: {
+    width: 180,
+    height: 50,
+    resizeMode: 'contain',
+
+  },
   item: {
     flexDirection: 'row',
     alignItems: 'center',
