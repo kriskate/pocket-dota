9a61c2c004567cd19aaff7e299340ec2185380b2
kristi <kristi.stanescu@gmail.com>
2018-08-31 13:21:04 +0300

    completed the list of items that have spelling errors in item_builds

--- src/components/Hero/ItemBuild.js
+++ src/components/Hero/ItemBuild.js
@@ -9,7 +9,45 @@ import { url } from '../../constants/Data';
 import Layout from '../../constants/Layout';
 import Logger from '../../utils/Logger';
 
-const skip_items = ['']
+const rename_items = {
+  "item_iron_talon": "deprecated",
+  "item_poor_mans_shield": "deprecated",
+
+  "item_ghost_scepter": "item_ghost",
+  "item_hand_midas": "item_hand_of_midas",
+  "item_manta_style": "item_manta",
+  "item_moonshard": "item_moon_shard",
+  "item_desolater": "item_desolator",
+  "item_item_desolator": "item_desolator",
+  "item_dagon_5": "item_dagon",
+  "item_necronomicon_1": "item_necronomicon",
+  "item_necronomicon_3": "item_necronomicon",
+  "item_necromonicon_3": "item_necronomicon",
+  "item_refresher_orb": "item_refresher",
+  "item_shadow_blade": "item_invis_sword",
+  "item_veil_of_dicord": "item_veil_of_discord",
+  "item_blood_stone": "item_bloodstone",
+  "item_eye_of_skadi": "item_skadi",
+  "item_assault_cuirass": "item_assault",
+  "item_crimson_time": "item_crimson_guard",
+  "item_spherer": "item_sphere",
+  "item_shivas": "item_shivas_guard",
+  "item_battle_fury": "item_bfury",
+  
+  "item_mango": "enchanted_mango",
+  "item_branch": "item_branches",
+  "item_ward_courier": "item_ward_sentry",
+  
+  "item_boots_of_speed": "item_boots",
+  "item_travel_boots_": "item_travel_boots",
+  "item_travel_boots_1": "item_travel_boots",
+  "item_travel_boots_2": "item_travel_boots",
+  "item_boots_of_travel_1": "item_travel_boots",
+
+  "item_diffusal_blade_2": "item_diffusal_blade",
+  "item_diffusal_2": "item_diffusal_blade",
+}
+
 @connect(state => ({
   game_items: state.wiki.items
 }))
@@ -17,7 +55,7 @@ export default class ItemBuild extends React.Component {
   render() {
     const item_builds = model_itembuild(this.props.item_builds);
     const itemSections = [];
-    const { game_items } = this.props;
+    const { game_items, hero_tag } = this.props;
 
     Object.keys(item_builds).forEach(moment => {
       if(!item_builds[moment] || item_builds[moment].length == 0) return;
@@ -27,15 +65,11 @@ export default class ItemBuild extends React.Component {
       
       //!data-point - add try catch
       item_builds[moment].forEach(npc_tag => {
-        if(npc_tag == 'item_dagon_5') npc_tag = 'item_dagon';
-        else if(npc_tag == 'item_necronomicon_3') npc_tag = 'item_necronomicon';
-        else if(npc_tag == 'item_travel_boots_2') npc_tag = 'item_travel_boots';
-        else if(npc_tag == 'item_refresher_orb') npc_tag = 'item_refresher';
-        else if(npc_tag == 'item_shadow_blade') npc_tag = 'item_invis_sword';
-        else if(npc_tag == 'item_veil_of_dicord') npc_tag = 'item_veil_of_discord';
+        if(rename_items[npc_tag]) npc_tag = rename_items[npc_tag];
+        if(npc_tag == 'deprecated') return;
 
         const item = game_items.find(({ tag }) => tag == npc_tag.replace('item_', ''));
-        if(!item) Logger.debug(npc_tag)
+        if(!item) Logger.debug(`could not find item ${hero_tag}: ${npc_tag}`);
         section.data.push(item);
       });
     });
