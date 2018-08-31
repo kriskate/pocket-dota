498dc256125fb81ecc28f5fa9a89546b83a12442
kristi <kristi.stanescu@gmail.com>
2018-08-31 13:34:46 +0300

    (ammend) updater does not need to know the loading status

--- src/reducers/update.js
+++ src/reducers/update.js
@@ -5,20 +5,15 @@ const ActionTypes = {
   REDOWNLOAD: 'REDOWNLOAD',
   UPDATE: 'UPDATE',
   DONE: 'DONE',
-
-  LOADING_DONE: 'LOADING_DONE',
 }
 export const Actions = {
   download: payload => ({ type: ActionTypes.DOWNLOAD }),
   redownload: payload => ({ type: ActionTypes.REDOWNLOAD }),
   update: payload => ({ type: ActionTypes.UPDATE }),
   done: payload => ({ type: ActionTypes.DONE }),
-
-  loadingDone: payload => ({ type: ActionTypes.LOADING_DONE }),
 }
 
 export const initialState = {
-  loading: true,
   downloading: false,
   downloadReason: '',
 };
@@ -33,8 +28,6 @@ export default function reducer(state=initialState, action) {
       return { downloading: true, downloadReason: DOWNLOAD_REASONS.UPDATE };
     case ActionTypes.DONE:
       return { downloading: false, downloadReason: '' };
-    case ActionTypes.LOADING_DONE:
-      return { ...state, loading: false };
     default:
       return state;
   }
