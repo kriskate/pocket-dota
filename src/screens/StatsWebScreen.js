4e149e77a7ed4789cbc2dd77d70ee81e2806937c
kristi <kristi.stanescu@gmail.com>
2018-09-01 09:09:36 +0300

    StatsWebScreen - IOS injectedJavascript fix

--- src/screens/StatsWebScreen.js
+++ src/screens/StatsWebScreen.js
@@ -17,17 +17,11 @@ const bkC = `background-color:${Colors.dota_ui1}!important;`;
 const bkCL = `background-color:${Colors.dota_ui1_light}!important;`;
 const bkC2 = `background-color:${Colors.dota_ui2}!important;`;
 const jsString = `
-  var css = "
-    #root { ${bkC2} background-image:none; }
-    ul { ${bkC} }
-    .gauge-container { ${bkC} }
-    thead { ${bkCL} }
-  ";
+  var css = "#root { ${bkC2} background-image:none; } ul { ${bkC} } .gauge-container { ${bkC} } thead { ${bkCL} }";
   var head = document.getElementsByTagName("head")[0];
   var style = document.createElement("style");
   style.appendChild(document.createTextNode(css));
   head.appendChild(style);
-
 
   var main = document.getElementById("root").childNodes[0];
 
@@ -35,7 +29,6 @@ const jsString = `
   main.childNodes[main.childNodes.length-1].remove();
   main.childNodes[1].remove();
   main.childNodes[0].remove();
-
 `
 
 const refs = { view: {} };
